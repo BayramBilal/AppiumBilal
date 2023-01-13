15a7a0e74f3eb00fab6e6ac1f6c2721d3d2fce6a
Bilal <105069025+BayramBilal@users.noreply.github.com>
2023-01-13 12:36:03 +0300

    Initial commit

---
+++ src/test/java/AppiumFirst/AppiumBasics.java
@@ -0,0 +1,28 @@
+package AppiumFirst;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class AppiumBasics {
+
+	@Test
+	public void AppiumTest() throws MalformedURLException {
+
+		//Android Driver, IOSDriver
+		//Appium code --> Appium Server --> Mobile
+
+		UiAutomator2Options options=  new UiAutomator2Options();
+		options.setDeviceName("Phone1");
+		options.setApp("C:\\Users\\Selim\\IdeaProjects\\AppiumBilal\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+
+		// Actual automation
+	}
+
+}
