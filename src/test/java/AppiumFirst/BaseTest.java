f46c54398acc4ca40debb9a2f2e9d03e0da78e69
Bilal <105069025+BayramBilal@users.noreply.github.com>
2023-02-02 16:12:27 +0300

    BrowserBaseTest adn browser android test added.

--- src/test/java/AppiumFirst/BaseTest.java
+++ src/test/java/AppiumFirst/BaseTest.java
@@ -30,10 +30,12 @@ public class BaseTest {
         serviceBuilder.start();
 
         UiAutomator2Options options=  new UiAutomator2Options();
-        options.setChromedriverExecutable("C:\\Users\\Selim\\chromedriver_win32\\chromedriver.exe");
         options.setDeviceName("Phone1");
+//        options.setDeviceName("Android Device");
+
+        options.setChromedriverExecutable("C:\\Users\\Selim\\chromedriver_win32\\chromedriver.exe");
         options.setApp("C:\\Users\\Selim\\IdeaProjects\\AppiumBilal\\src\\test\\java\\resources\\ApiDemos-debug.apk");
-        options.setApp("C:\\Users\\Selim\\IdeaProjects\\AppiumBilal\\src\\test\\java\\resources\\General-store.apk");
+//        options.setApp("C:\\Users\\Selim\\IdeaProjects\\AppiumBilal\\src\\test\\java\\resources\\General-store.apk");
         // Actual automation
         // id, xpath, accessibiltyId, className, androidUIAutomator
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
